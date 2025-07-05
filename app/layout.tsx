72c80ae321cbeba34719ddca5ccf21ef43aa0bc8
Lampz <lampazaj@gmail.com>
2025-07-05 14:30:13 +0800

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "BookWise",
+  description:
+    "BookWise is a book borrowing university library management solution.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
--- data.js
+++
@@ -1,2 +0,0 @@
-export const API_KEY = "18d913b1f0c245e7bf33129f14c4aa6f";
-export const COUNTRY = "us";
